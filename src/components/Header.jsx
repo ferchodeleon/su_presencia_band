2e388012c92bbb032daece44ae5524048636ed83
Fernando De Leon Gil <gasnare@hotmail.com>
2024-04-03 16:31:55 -0500

    Ajuste para modo responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
 import Logo from "../assets/logo/logo_white.svg";
 import "../style/header.css";
 
 export const Header = ({ color }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isOpen = isMenuOpen ? "open" : "";
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <header className="header-container" style={{ background: color ?? color }}>
       <div className="image-logo">
         <img src={Logo} alt="imagen en logo de la banda su presencia worship" />
       </div>
+      <button className={`burger ${isOpen}`} onClick={toggleMenu}></button>
+      <div className={`header-background ${isOpen}`}></div>
       <div className="header-menu">
         <ul>
           <li>
