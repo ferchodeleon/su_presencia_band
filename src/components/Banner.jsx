b6ff75a0c3503aed191ce57a16b9205aabd8d974
Fernando De Leon Gil <gasnare@hotmail.com>
2024-04-05 17:43:17 -0500

    ajuste para mostrar los datos desde un json

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,85 +1,89 @@
+import { useState } from "react";
+
 import firstImage from "../assets/img/tienes_el_control.jpg";
 import albumTienesControl from "../assets/img/album_tienes_el_control.jpg";
+import vegaTuReino from "../assets/img/venga_tu_reino.jpg";
+import pesadilla from "../assets/img/pesadilla.jpg";
 
 import "../style/banner.css";
 
-export const Banner = () => {
-  const slider = document.querySelector(".banner-slider");
+const data = [
+  {
+    title: "<span>Álbum tienes</span> el control",
+    description: "Tienes el control",
+    button: "Ver concierto",
+    link: "",
+    image: pesadilla,
+  },
+  {
+    title: "<span>Tienes el</span> control",
+    description: "Último lanzamiento",
+    button: "Ver video",
+    link: "",
+    image: firstImage,
+  },
+  {
+    title: "<span>Texto de</span> título",
+    description: "Descripción corta",
+    button: "Botón",
+    link: "",
+    image: albumTienesControl,
+  },
+  {
+    title: "<span>Álbum tienes</span> el control",
+    description: "Tienes el control",
+    button: "Ver concierto",
+    link: "",
+    image: vegaTuReino,
+  },
+];
 
-  function activate(e) {
-    const items = document.querySelectorAll("banner-item");
-  }
+export const Banner = () => {
+  const [, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    const slider = document.querySelector(".banner-slider");
+    const items = document.querySelectorAll(".banner-item");
+
+    const texto = "Album tienes el control de mi";
+    const textoModificado = texto.split(/(\s{1,})/g);
+    console.log(textoModificado);
+
+    setCurrentIndex(
+      () => slider.append(items[0]) // Agregar la siguiente imagen al contenedor del slider
+    );
+  };
+
+  const prevSlide = () => {
+    const slider = document.querySelector(".banner-slider");
+    const items = document.querySelectorAll(".banner-item");
+    setCurrentIndex(() => slider.prepend(items[items.length - 1])); // Agregar la primera imagen al final del contenedor del slider
+  };
 
   return (
     <div className="banner-container">
       <div className="banner-main">
         <ul className="banner-slider">
-          <li
-            className="banner-item"
-            style={{
-              backgroundImage: `url(${albumTienesControl})`,
-            }}
-          >
-            <div className="content">
-              <p className="title">
-                <span>Álbum tienes</span> el control
-              </p>
-              <p className="description">Tienes el control</p>
-              <button>Ver concierto</button>
-            </div>
-          </li>
-          <li
-            className="banner-item"
-            style={{
-              backgroundImage: `url(${firstImage})`,
-            }}
-          >
-            <div className="content">
-              <p className="title">
-                <span>Tienes el</span> control
-              </p>
-              <p className="description">Último lanzamiento</p>
-              <button>Ver video</button>
-            </div>
-          </li>
-          <li
-            className="banner-item"
-            style={{
-              backgroundImage: `url(${albumTienesControl})`,
-            }}
-          >
-            <div className="content">
-              <p className="title">
-                <span>Álbum tienes</span> el control
-              </p>
-              <p className="description">Disponible en todas las plataformas</p>
-              <button>Ver video</button>
-            </div>
-          </li>
-          <li
-            className="banner-item"
-            style={{
-              backgroundImage: `url(${albumTienesControl})`,
-            }}
-          >
-            <div className="content">
-              <p className="title">
-                <span>Álbum tienes</span> el control
-              </p>
-              <p className="description">Disponible en todas las plataformas</p>
-              <button>Ver video</button>
-            </div>
-          </li>
+          {data.map((info, index) => (
+            <li
+              key={index}
+              className="banner-item"
+              style={{ backgroundImage: `url(${info.image})` }}
+            >
+              <div className="content">
+                <p
+                  className="title"
+                  dangerouslySetInnerHTML={{ __html: info.title }}
+                ></p>
+                <p className="description">{info.description}</p>
+                <button onClick={nextSlide}>{info.button}</button>
+              </div>
+            </li>
+          ))}
         </ul>
         <nav className="nav">
-          <span
-            className="btn prev"
-            // name="arrow-back-outline"
-          ></span>
-          <span
-            className="btn next"
-            // name="arrow-forward-outline"
-          ></span>
+          <button className="btn prev" onClick={prevSlide}></button>
+          <button className="btn next" onClick={nextSlide}></button>
         </nav>
       </div>
     </div>
