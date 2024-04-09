5e25b0c2f00c9cc7703f1f61a3d04fa6765cca6e
Fernando De Leon Gil <gasnare@hotmail.com>
2024-04-09 14:26:34 -0500

    Sección de banner para promociones

---
+++ src/components/PromotionBanner.jsx
@@ -0,0 +1,31 @@
+import background from "../assets/img/background-promotion.jpg";
+import soyPerdonado from "../assets/img/portada-soy-perdonado.jpg";
+
+import "../style/promotion_banner.css";
+
+export const PromotionBanner = () => {
+  return (
+    <>
+      <section className="promotion-container">
+        <div
+          className="promotion-background"
+          style={{ backgroundImage: `url(${background})` }}
+        />
+        <div className="promotion-info">
+          <div className="info-container">
+            <img
+              src={soyPerdonado}
+              alt="Imagen de canción llamada Soy perdonado"
+            />
+            <div>
+              <p>
+                Lanzamiento <br /> soy perdonado
+              </p>
+              <button>Ver más</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
