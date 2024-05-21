b30851ec5d34d3d9aa610520fb08307d97d03ed7
Fernando De Leon Gil <gasnare@hotmail.com>
2024-05-21 16:01:31 -0500

    cambio para cargar la página en servidor de prueba

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import { Banda } from "./pages/Banda";
 
 import "./styles/main.css";
@@ -7,24 +7,28 @@ import { Header } from "./components/Header";
 import { Error404 } from "./pages/Error404";
 import { Footer } from "./components/Footer";
 import { DiscographyPage } from "./pages/DiscographyPage";
+import ScrollToTop from "./components/ScrollTop";
 
 function App() {
   return (
     <>
       {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
-      <Header
-      // color={"rgba(6, 6, 6, 0.5)"}
-      />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/band" element={<Banda />} />
-        <Route path="/discography" element={<DiscographyPage />} />
-        <Route path="/videos" element={<Home />} />
-        <Route path="/resourse" element={<Home />} />
-        <Route path="/contact" element={<Home />} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-      <Footer />
+      <HashRouter>
+        <ScrollToTop />
+        <Header
+        // color={"rgba(6, 6, 6, 0.5)"}
+        />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/band" element={<Banda />} />
+          <Route path="/discography" element={<DiscographyPage />} />
+          <Route path="/videos" element={<Home />} />
+          <Route path="/resourse" element={<Home />} />
+          <Route path="/contact" element={<Home />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+        <Footer />
+      </HashRouter>
       {/* </BrowserRouter> */}
     </>
   );
