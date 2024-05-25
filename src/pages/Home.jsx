33f7ea5bc62818751fa2341930898ba8edef6eec
Fernando De Leon Gil <gasnare@hotmail.com>
2024-05-25 11:00:55 -0500

    Se agrega el componente loading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,24 @@ import { BannerTutorial } from "../components/BannerTutorial";
 import { PromotionBanner } from "../components/PromotionBanner";
 
 import { dataMainBanner } from "../assets/local_data";
+import backgroundPromotion from "../assets/img/background-promotion.webp";
+import soyPerdonadoImage from "../assets/img/portada-soy-perdonado.webp";
+import Loading from "../components/Loading";
 
 export const Home = () => {
   return (
-    <>
-      <Banner data={dataMainBanner} />
-      <PromotionBanner />
-      <BannerTutorial />
-    </>
+    <Loading
+      children={
+        <>
+          <Banner data={dataMainBanner} />
+          <PromotionBanner
+            background={backgroundPromotion}
+            soyPerdonado={soyPerdonadoImage}
+          />
+          <BannerTutorial />
+        </>
+      }
+      imageUrls={[dataMainBanner.image, backgroundPromotion, soyPerdonadoImage]}
+    />
   );
 };
