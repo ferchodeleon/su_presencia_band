63f3aa3e231aa035e05098a5c20f6d3dbd4423d3
Fernando De Leon Gil <gasnare@hotmail.com>
2024-05-28 08:49:47 -0500

    Se integra el componente loading

--- src/pages/DiscographyPage.jsx
+++ src/pages/DiscographyPage.jsx
@@ -1,25 +1,31 @@
 import { discography } from "../assets/local_data";
 import { Discography } from "../components/Discography";
+import Loading from "../components/Loading";
 
 import "../styles/discography_page.css";
 
 export const DiscographyPage = () => {
   return (
-    <div className="main-discography">
-      <h2>Discografía</h2>
-      <div className="discography-page-container">
-        {discography.map((album) => (
-          <Discography
-            key={album.id}
-            id={album.id}
-            discographyName={album.nameAlbum}
-            year={album.year}
-            totalSongs={album.totalSongs}
-            image={album.image}
-            songs={album.songs}
-          />
-        ))}
-      </div>
-    </div>
+    <Loading
+      children={
+        <div className="main-discography">
+          <h2>Discografía</h2>
+          <div className="discography-page-container">
+            {discography.map((album) => (
+              <Discography
+                key={album.id}
+                id={album.id}
+                discographyName={album.nameAlbum}
+                year={album.year}
+                totalSongs={album.totalSongs}
+                image={album.image}
+                songs={album.songs}
+              />
+            ))}
+          </div>
+        </div>
+      }
+      imageUrls={[discography.image]}
+    />
   );
 };
