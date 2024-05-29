f88a4d30c86a190f606e20130748af5678c9e65f
Fernando De Leon Gil <gasnare@hotmail.com>
2024-05-29 14:54:21 -0500

    Se agregan los links de los iconos de streaming

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import Spotify from "../assets/icons/spotify.svg";
 import Youtube from "../assets/icons/youtube_music.svg";
 import Apple from "../assets/icons/apple_music.svg";
 import "../styles/footer.css";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
@@ -15,21 +16,33 @@ export const Footer = () => {
         <span>Contacto personal:</span> abetancur@supresencia.com
       </p>
       <div className="footer-music">
-        <img
-          className="footer-spotify"
-          src={Spotify}
-          alt="Icono de Spotify en color café"
-        />
-        <img
-          className="footer-youtube"
-          src={Youtube}
-          alt="Icono de youtube music en color café"
-        />
-        <img
-          className="footer-apple"
-          src={Apple}
-          alt="Icono de apple music en color café"
-        />
+        <Link
+          target="__blank"
+          to="https://open.spotify.com/intl-es/artist/2gaFnEQydJdWNkT17NLZm3?si=EBgkqmKrQjybnOlEz8vq9g"
+        >
+          <img
+            className="footer-spotify"
+            src={Spotify}
+            alt="Icono de Spotify en color café"
+          />
+        </Link>
+        <Link target="__blank" to="https://www.youtube.com/@SuPresenciaWorship">
+          <img
+            className="footer-youtube"
+            src={Youtube}
+            alt="Icono de youtube music en color café"
+          />
+        </Link>
+        <Link
+          target="__blank"
+          to="https://music.apple.com/us/artist/su-presencia/322571579"
+        >
+          <img
+            className="footer-apple"
+            src={Apple}
+            alt="Icono de apple music en color café"
+          />
+        </Link>
       </div>
     </footer>
   );
