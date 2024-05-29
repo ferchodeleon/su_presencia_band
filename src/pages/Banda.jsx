ced9b8a5dcd97302a96b703d3fd8d3f2fce1a7ce
Fernando De Leon Gil <gasnare@hotmail.com>
2024-05-29 14:54:57 -0500

    Cambio para loading de imagenes

--- src/pages/Banda.jsx
+++ src/pages/Banda.jsx
@@ -5,6 +5,8 @@ import { PortraitBanda } from "../components/PortraitBanda";
 import Portrait from "../assets/img/banda/portrait.jpg";
 import Loading from "../components/Loading";
 
+import { dataBand } from "../assets/local_data";
+
 export const Banda = () => {
   return (
     <Loading
@@ -12,7 +14,7 @@ export const Banda = () => {
         <>
           <Header menuSelect="La banda" />
           <PortraitBanda ImagePortrait={Portrait} />
-          <History />
+          <History dataBand={dataBand} />
         </>
       }
       imageUrls={[Portrait]}
